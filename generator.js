9e6ccaa3d81594134185126c2f465f7a1f5ed851
Unnamed Developer <unnamed.a.dev@gmail.com>
2018-01-20 17:28:01 +0100

    v0.0.3
    HTML
    + Added structure of preview
    JAVASCRIPT
    + Added code that provide function to refresh preview of user values on left side and code that will provide refresh of preview
    CSS
    + Created style properties for preview div

--- generator.js
+++ generator.js
@@ -2,4 +2,49 @@
 
 $(document).ready(function(){
     
+    //const
+    
+    //vars
+    
+    //main
+    addInputEvents();
+    
+    //fxs
+    function addInputEvents(){
+        var validDiv = document.getElementById("configuration");
+        console.log(validDiv);
+        
+        var inputs = validDiv.getElementsByTagName("input");
+        
+        for(var i=0;i<inputs.length;i++){
+            inputs[i].addEventListener("input", function(){
+                refreshValue(this);
+            });
+        }
+    }
+    
+    function refreshValue(obj){
+        var newId = obj.id + "Value";
+        var newObj = document.getElementById(newId);
+        newObj.innerHTML = obj.value + " px";
+        
+        var previewObj = document.getElementById("exampleDiv");
+        
+        refreshPreview(previewObj);
+    }
+    
+    function refreshPreview(previewObj){
+        
+        var moveX = document.getElementById("sMoveX").value;
+        var moveY = document.getElementById("sMoveY").value;
+        var blur = document.getElementById("sBlur").value;
+        var size = document.getElementById("sSize").value;
+        var shadowColor = document.getElementById("sShadowColor").value;
+        var backgroundColor = document.getElementById("sBackgroundColor").value;
+        
+        previewObj.style.background = "#"+backgroundColor;
+        
+        previewObj.style.boxShadow = moveX+moveY+blur+size+"#"+shadowColor;
+        
+    }
 });
